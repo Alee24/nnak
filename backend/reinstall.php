3d0fcb0c293d804b4ca17c30ed56f6d18b4725fb
Alee24 <mettoalex@gmail.com>
2026-09-23 20:05:41 +0300

    fix(reinstall): fix case sensitive file path for Database.php on Linux filesystem

--- backend/reinstall.php
+++ backend/reinstall.php
@@ -6,7 +6,7 @@ ob_start();
  */
 
 require_once __DIR__ . '/config/config.php';
-require_once __DIR__ . '/config/database.php';
+require_once __DIR__ . '/config/Database.php';
 require_once __DIR__ . '/utils/Normalization.php';
 require_once __DIR__ . '/utils/Auth.php';
 
